b1928a7288f31929baec66e9c7255c416fbc7bfa
idsok <i.sokhona@salviadeveloppement.com>
2017-03-17 10:32:44 +0100

    initial commit

---
+++ src/library.ts
@@ -0,0 +1,19 @@
+declare var $: any;
+declare var joint: any;
+declare var _: any;
+
+namespace Library {
+    export module diagram {
+        // class et interface du lib
+    }
+
+
+    namespace Test {
+
+        function testInit() {
+
+        }
+
+        testInit();
+    }
+}
