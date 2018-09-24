7d96a7bbd0490d3d4335d39b4d10f5771bbcd188
Idrissa SOKHONA <rokysaroi@gmail.com>
2018-09-24 20:22:12 +0200

    md

--- library.js
+++ library.js
@@ -2,7 +2,7 @@ var Library;
 (function (Library) {
     var utils;
     (function (utils) {
-        class Combinaison {
+        class Generate {
             static generateCombinaisons(a) {
                 var fn = function (n, src, got, all) {
                     if (n == 0) {
@@ -34,8 +34,8 @@ var Library;
                 }
                 return res;
             }
-            static getCombinaisons(ensemble, k, critereCtr) {
-                let allCombinaisons = Combinaison.generateCombinaisons(ensemble);
+            static getCombinaisons(ensemble, k, criteresCtr) {
+                let allCombinaisons = Generate.generateCombinaisons(ensemble);
                 let limitedCombinaisonsbyK;
                 if (k) {
                     limitedCombinaisonsbyK = [];
@@ -48,10 +48,10 @@ var Library;
                 else
                     limitedCombinaisonsbyK = allCombinaisons;
                 let limitedCombinaisonsbyCritereCtr;
-                if (critereCtr) {
+                if (criteresCtr) {
                     limitedCombinaisonsbyCritereCtr = [];
                     limitedCombinaisonsbyK.forEach(cb => {
-                        cb = critereCtr(cb);
+                        cb = criteresCtr(cb);
                         if (cb)
                             limitedCombinaisonsbyCritereCtr.push(cb);
                     });
@@ -60,17 +60,151 @@ var Library;
                     limitedCombinaisonsbyCritereCtr = limitedCombinaisonsbyK;
                 return limitedCombinaisonsbyCritereCtr;
             }
+            static getObjectCombinaisons(ensemble, k, criteresCtr) {
+                let that = this;
+                let ids = [];
+                ensemble = ensemble.map((v, i) => {
+                    v.id = i;
+                    ids.push(v.id);
+                    return v;
+                });
+                let combinaisonsIds = Generate.getCombinaisons(ids, k);
+                let combinaisonsObject = combinaisonsIds.map(cb => {
+                    cb.forEach((v, i) => {
+                        cb[i] = ensemble[v];
+                    });
+                    return cb;
+                });
+                let res = [];
+                combinaisonsObject.forEach(cb => {
+                    cb = criteresCtr(cb);
+                    if (cb)
+                        res.push(cb);
+                });
+                return res;
+            }
         }
-        utils.Combinaison = Combinaison;
+        utils.Generate = Generate;
+        class UX {
+            static toString(combinaisons, props) {
+                combinaisons = combinaisons || [];
+                props = props || ["nom"];
+                let cbRes = [];
+                combinaisons.forEach((cb, index) => {
+                    let eRes = [];
+                    cb.forEach(e => {
+                        let pRes = [];
+                        for (let i = 1; i < props.length; i++) {
+                            pRes.push(props[i] + " = " + e[props[i]]);
+                        }
+                        eRes.push(e[props[0]] + (pRes.length ? "(" + pRes.join(", ") + ")" : ""));
+                    });
+                    cbRes.push("[ " + eRes.join(" , ") + " ]");
+                });
+                return "{\n\t" + cbRes.join("\n\t") + "\n}";
+            }
+        }
+        utils.UX = UX;
+        class AppData {
+            constructor(config) {
+                let that = this;
+                that.config = config;
+            }
+        }
+        utils.AppData = AppData;
     })(utils = Library.utils || (Library.utils = {}));
     var Test;
     (function (Test) {
         function testInit() {
-            console.log(Library.utils.Combinaison.countCombinaisons(4, 5));
-            console.log(Library.utils.Combinaison.getCombinaisons([1, 2, 3, 4, 5], 4, c => {
-                if (c[0] == 1)
+            let data = {
+                criteres: {
+                    age: [">= 10"],
+                    couleur: ["black"]
+                },
+                couleurs: {
+                    black: {
+                        title: "Black"
+                    },
+                    blanc: {
+                        title: "Blanc"
+                    },
+                    melange: {
+                        title: "Melange"
+                    }
+                },
+                genres: {
+                    m: {
+                        title: "Male",
+                    },
+                    f: {
+                        title: "Femelle"
+                    },
+                    h: {
+                        title: "Hongre"
+                    }
+                },
+                groups: {
+                    default: {
+                        title: "Défault"
+                    },
+                    paire: {
+                        title: "Paire"
+                    },
+                    impaire: {
+                        name: "impaire",
+                        title: "Impaire"
+                    }
+                },
+                ensemble: [
+                    {
+                        nom: "A",
+                        age: 8,
+                        genre: "m",
+                        couleur: "black"
+                    },
+                    {
+                        nom: "B",
+                        age: 10,
+                        genre: "f",
+                        couleur: "black"
+                    },
+                    {
+                        nom: "C",
+                        age: 15,
+                        genre: "h",
+                        couleur: "melange"
+                    },
+                    {
+                        nom: "D",
+                        age: 20,
+                        genre: "m",
+                        couleur: "blanc"
+                    },
+                    {
+                        nom: "E",
+                        age: 21,
+                        genre: "m",
+                        couleur: "blanc"
+                    }
+                ],
+                cordes: ["A", "B", "C", "D", "E"]
+            };
+            let n = 5;
+            let k = 4;
+            console.log(Library.utils.Generate.countCombinaisons(k, n));
+            let combinaisons = Library.utils.Generate.getObjectCombinaisons(data.ensemble, k, c => {
+                let ok = true;
+                let cv = 0;
+                c.every(e => {
+                    if (e.nom == "C" || e.nom == "B") {
+                        cv++;
+                    }
+                    return true;
+                });
+                if (ok)
                     return c;
-            }));
+            });
+            console.log(Library.utils.UX.toString(combinaisons, ["nom"]));
         }
         testInit();
     })(Test || (Test = {}));
--- src/library.ts
+++ src/library.ts
@@ -5,7 +5,24 @@ declare var _: any;
 namespace Library {
     export module utils {
         // class et interface du lib
-        export class Combinaison {
+        export interface Ensemble {
+            id?: number;
+            nom: string;
+            age?: number;
+            genre?: string;
+            couleur?: string;
+        }
+
+        export interface Config {
+            couleurs: any; // { name: { title: string } }
+            genres: any; // { name: { title: string } }
+            groups: any; // { name: { title: string } };
+            cordes: string[];
+            ensemble: Ensemble[];
+            criteres: any;
+        }
+
+        export class Generate {
             // javascript
             public static generateCombinaisons(a) { // a = new Array(1,2)
                 var fn = function (n, src, got, all) {
@@ -36,8 +53,8 @@ namespace Library {
                 }
                 return res;
             }
-            public static getCombinaisons(ensemble: any [], k?: number, criteresCtr?: Function) {
-                let allCombinaisons = Combinaison.generateCombinaisons(ensemble);
+            public static getCombinaisons(ensemble: any[], k?: number, criteresCtr?: Function) {
+                let allCombinaisons = Generate.generateCombinaisons(ensemble);
                 let limitedCombinaisonsbyK;
                 if (k) {
                     limitedCombinaisonsbyK = [];
@@ -61,90 +78,165 @@ namespace Library {
                     limitedCombinaisonsbyCritereCtr = limitedCombinaisonsbyK;
                 return limitedCombinaisonsbyCritereCtr;
             }
-    }
-
-}
-
-
-namespace Test {
-
-    function testInit() {
-        let data = {
-            couleurs: {
-                black: {
-                    title: "Black"
-                },
-                blanc: {
-                    title: "Blanc"
-                },
-                melange: {
-                    title: "Melange"
-                }
-            },
-            genres: {
-                m : {
-                    title: "Male",
-                },
-                f: {
-                    title: "Femelle"
-                },
-                h: {
-                    title: "Hongre"
-                }
-            },
-            groups: {
-                default: {
-                    title: "Défault"
-                },
-                paire: {
-                    title: "Paire"
-                },
-                impaire:{
-                    name: "impaire",
-                    title: "Impaire"
-                }
-            },
-            ensemble: [
-                {
-                    nom: "A",
-                    age: 8,
-                    genre: "m",
-                    couleur: "black"
-                },
-                {
-                    nom: "B",
-                    age: 10,
-                    genre: "f",
-                    couleur: "black"
-                },
-                {
-                    nom: "C",
-                    age: 15,
-                    genre: "h",
-                    couleur: "melange"
-                },
-                {
-                    nom: "D",
-                    age: 20,
-                    genre: "m",
-                    couleur: "blanc"
-                },
-                {
-                    nom: "E",
-                    age: 21,
-                    genre: "m",
-                    couleur: "blanc"
-                }
-            ]
+            public static  getObjectCombinaisons(ensemble: Ensemble[], k?: number, criteresCtr?: Function) {
+                let that = this;
+                // Identifier les éléments de l'ensemble
+                let ids = [];
+                ensemble = ensemble.map((v, i) => {
+                    v.id = i;
+                    ids.push(v.id);
+                    return v;
+                });
+                // Générer les combinaisons
+                let combinaisonsIds = Generate.getCombinaisons(ids, k);
+                // Compléter les informations de combinaisons
+                let combinaisonsObject = combinaisonsIds.map(cb => {
+                    cb.forEach((v, i) => {
+                        cb[i] = ensemble[v];
+                    });
+                    return cb;
+                });
+                // Appliquer les critères
+                let res = [];
+                combinaisonsObject.forEach(cb => {
+                    cb = criteresCtr(cb);
+                    if (cb) res.push(cb);
+                });
+                return res;
+            }
         }
-        console.log(Library.utils.Combinaison.countCombinaisons(4, 5));
-        //console.log(Library.utils.Combinaison.combinaisons([1, 2, 3, 4, 5]));
-        console.log(Library.utils.Combinaison.getCombinaisons([1, 2, 3, 4, 5], 4, c => {
-            if (c[0] == 1) 
-                return c;
-        }));
+
+        export class UX {
+            public static toString(combinaisons: any[], props?: string[]): string {
+                combinaisons = combinaisons || [];
+                props = props || ["nom"];
+                let cbRes = [];
+                combinaisons.forEach((cb, index) => {
+                    let eRes = [];
+                    cb.forEach(e => {
+                        let pRes = [];
+                        for (let i = 1; i < props.length; i++) {
+                            pRes.push(props[i] +" = "+ e[props[i]]);
+                        }
+                        eRes.push(e[props[0]] + ( pRes.length ? "("+ pRes.join(", ") +")" : ""));
+                    });
+                    cbRes.push("[ "+ eRes.join(" , ") +" ]");
+                });
+                return "{\n\t"+ cbRes.join("\n\t") +"\n}";
+            }
+        }
+
+        export class AppData {
+            private config: Library.utils.Config;
+            constructor(config?: Config) {
+                let that = this;
+                that.config = config;
+            }
+        }
     }
 
-    testInit();
-}
+
+    namespace Test {
+
+        function testInit() {
+            let data: Library.utils.Config = {
+                criteres: {
+                    age: [">= 10"],
+                    couleur: ["black"]
+                },
+                couleurs: {
+                    black: {
+                        title: "Black"
+                    },
+                    blanc: {
+                        title: "Blanc"
+                    },
+                    melange: {
+                        title: "Melange"
+                    }
+                },
+                genres: {
+                    m: {
+                        title: "Male",
+                    },
+                    f: {
+                        title: "Femelle"
+                    },
+                    h: {
+                        title: "Hongre"
+                    }
+                },
+                groups: {
+                    default: {
+                        title: "Défault"
+                    },
+                    paire: {
+                        title: "Paire"
+                    },
+                    impaire: {
+                        name: "impaire",
+                        title: "Impaire"
+                    }
+                },
+                ensemble: [
+                    {
+                        nom: "A",
+                        age: 8,
+                        genre: "m",
+                        couleur: "black",
+                        corde: 1
+                    },
+                    {
+                        nom: "B",
+                        age: 10,
+                        genre: "f",
+                        couleur: "black",
+                        corde: 3
+                    },
+                    {
+                        nom: "C",
+                        age: 15,
+                        genre: "h",
+                        couleur: "melange"
+                    },
+                    {
+                        nom: "D",
+                        age: 20,
+                        genre: "m",
+                        couleur: "blanc"
+                    },
+                    {
+                        nom: "E",
+                        age: 21,
+                        genre: "m",
+                        couleur: "blanc"
+                    }
+                ],
+                cordes: [3, 1, 4, 5, 2]
+            }
+            let n = 5;
+            let k = 4;
+            console.log(Library.utils.Generate.countCombinaisons(k, n));
+            //console.log(Library.utils.Combinaison.combinaisons([1, 2, 3, 4, 5]));
+            let combinaisons = Library.utils.Generate.getObjectCombinaisons(data.ensemble, k, c => {
+                let ok: boolean = true;
+                let cv = 0;
+                c.every(e => { 
+
+                    //if (e.age < 10) {
+                    //    ok = false;
+                    //    return false;
+                    //}
+                    if (e.nom == "C" || e.nom == "B") {
+                        cv++;
+                    }
+                    return true;
+                });
+                if (ok) return c;
+            });
+            console.log(Library.utils.UX.toString(combinaisons, ["nom"]));
+        }
+
+        testInit();
+    }
 }
